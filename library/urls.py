b30d4d31d3da809c2f8068fe2829b0e722cc3a31
gracejmoses <gracejmoses@gmail.com>
2024-07-10 15:33:06 +0100

    added master templates

---
+++ library/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.welcome, name='welcome page'),
+    path('books/', views.books, name='Books'),
+    path('users/',views.users, name='users'),
+]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def welcome(request):
+    # return HttpResponse("Welcom to e-library")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+
+def books(request):
+    # return HttpResponse("Books")
+    template = loader.get_template('books.html')
+    return HttpResponse(template.render())
+
+def users(request):
+    # return HttpResponse("users")
+    template = loader.get_template('users.html')
+    return HttpResponse(template.render())
